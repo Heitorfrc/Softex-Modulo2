52f7708cfcad486635ae187ed416ba55649a86d6
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2022-12-19 16:39:33 -0300

    Update exemplo-array.js

--- exemplo-array.js
+++ exemplo-array.js
@@ -1,3 +1,10 @@
+/* Desenvolva um código e crie nele:
+
+- um objeto com, no mínimo, três propriedades;
+- um array de tamanho três no mínimo;
+- duas funções, a primeira lista as propriedades do objeto e a segunda, os elementos do array.
+*/
+
 let filme = {
     nome: "matrix",
     ano: 1999,
